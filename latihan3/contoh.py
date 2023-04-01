07e2abfdd56b79de070ff935bffde6c85dc8802f
AlipTaufik <127857070+AlipTaufik@users.noreply.github.com>
2023-04-01 20:42:22 +0700

    Add files via upload

---
+++ latihan3/contoh.py
@@ -0,0 +1,18 @@
+print(len("Hello")) # output: 5
+print(len([1, 2, 3])) # output: 3
+print(len((1, 2, 3))) # output: 3
+
+print(2 + 3) # output: 5
+print("Hello" + " World") # output: Hello World
+print([1, 2] + [3, 4]) # output: [1, 2, 3, 4
+
+print(max(2, 5)) # output: 5
+print(max([1, 2, 3])) # output: 3
+print(max("Hello")) # output: "o"
+
+a = [3, 1, 2]
+a.sort()
+print(a) # output: [1, 2, 3]
+b = ["c", "a", "b"]
+b.sort()
+print(b) # output: ["a", "b", "c"]
